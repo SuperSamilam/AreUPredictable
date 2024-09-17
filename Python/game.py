d45359c63df08213ce0c2e0cd3cbf240a3744f32
SuperSamilam <98377838+SuperSamilam@users.noreply.github.com>
2024-09-17 15:43:31 +0200

    test consitant window size

--- Python/game.py
+++ Python/game.py
@@ -3,17 +3,20 @@ import pygame
 screenWidth = 400
 screenHeight = 600
 
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+# screen = pygame.display.set_mode((screenWidth, screenHeight))
+screen = pygame.display.set_mode((screenWidth, screenHeight) ,pygame.SCALED)
 
 run = True
 
 white = (255,255,255)
 black = (0,0,0)
 
+baseRect = pygame.Rect((0,0,400,10))
+
 while run:
     
     screen.fill(white)
-    pygame.draw.rect(screen, black)
+    pygame.draw.rect(screen, black, baseRect)
     pygame.display.flip()
     
     for event in pygame.event.get():
